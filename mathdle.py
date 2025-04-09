2b0f0e477b5d7788ba9b6e0bc3dea072acd686d1
Evan <estrems.evan@gmail.com>
2025-04-09 17:03:09 +0200

    api connection + frontend update

--- mathdle.py
+++ mathdle.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, url_for
-from random import randint
+from flask import Flask, render_template, url_for, redirect, request
 import random
 from datasets import load_dataset
 from datetime import date
@@ -8,37 +7,33 @@ from dotenv import load_dotenv
 from mistralai import Mistral
 
 load_dotenv('api_keys.env')
+
 api_key = os.environ["MISTRAL_API_KEY"]
-model = "mistral-small-latest"
 
 client = Mistral(api_key=api_key)
-
-# ------------- Examples of AI use -------------
-chat_response = client.chat.complete(
-    model = model,
-    messages = [
-        {
-            "role": "user",
-            "content": "What is the best French cheese?",
-        },
-    ]
-)
-print(chat_response.choices[0].message.content)
-# -----------------------------------------------
 
 
 # Load dataset and chose a random question
 ds = load_dataset("HuggingFaceH4/MATH-500", split='test')
 q = random.choice(ds)
 
+print(q['solution'])
+
 app = Flask(__name__)
+
+# --------- Functions ---------
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/questions")
+@app.route("/questions", methods=['POST', 'GET'])
 def questions():
+    if request.method == 'POST':
+        user_response = request.form["user-response"]
+        print(user_response)
+        get_chat_response(user_response)
+    
     return render_template('questions.html', date=date.today(), question=q['problem'])
 
 @app.route("/about")
@@ -50,6 +45,21 @@ def skip_question():
     global q
     q = random.choice(ds)
     return q['problem']
+
+def get_chat_response(txt):
+    global q
+    prompt = "1. " + q["problem"] + "\n\n" + "2. " + q["solution"] + "\n\n" + "3. " + q['answer'] + '\n\n' + "4. " + txt
+    print(prompt)
+    chat_response = client.agents.complete(
+        agent_id = os.environ['MODEL'],
+        messages = [
+            {
+                "role": "user",
+                "content": prompt, # we might put it into a formatted string
+            },
+        ]
+    )
+    print(chat_response.choices[0].message.content)
 
 if __name__=="__main__":
     app.run()
